c2f8d30efebf540723eb0ff2ce46820d880878ab
sifiso mashengele <mashengele78@gmail.com>
2026-08-23 23:44:01 +0200

    Point /now at working root app.js

--- now/sw.js
+++ now/sw.js
@@ -1,28 +1,13 @@
-const CACHE = "pg-now-v20260823c";
-const PRECACHE = ["./index.html", "./app.js", "./manifest.webmanifest"];
-
+const CACHE = "pg-now-v20260823d";
+const PRECACHE = ["./index.html", "../app.js", "./manifest.webmanifest"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(PRECACHE)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener("fetch", (e) => {
   const req = e.request;
   if (req.method !== "GET") return;
-  e.respondWith(
-    fetch(req)
-      .then((res) => {
-        if (res && res.ok) {
-          const copy = res.clone();
-          caches.open(CACHE).then((c) => c.put(req, copy));
-        }
-        return res;
-      })
-      .catch(() => caches.match(req).then((hit) => hit || caches.match("./index.html")))
-  );
+  e.respondWith(fetch(req).then((res) => { if (res && res.ok) { const copy = res.clone(); caches.open(CACHE).then((c) => c.put(req, copy)); } return res; }).catch(() => caches.match(req).then((hit) => hit || caches.match("./index.html"))));
 });
